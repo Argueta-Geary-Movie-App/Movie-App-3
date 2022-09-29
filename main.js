f0fc93eec4c9bc25acd4d2876784af22f6170169
Casanova Geary <CasanovaGeary@Gmail.com>
2022-09-29 16:21:05 -0500

    HTML added Bootstrap CSS CDN; main.js added bootstrap card for const html.

--- main.js
+++ main.js
@@ -15,13 +15,19 @@ function fetchMovies() {
             console.log(data);
             const html = data.map(movie => {
                 return `
-                    <div id="card">
-                        <div class="movie">
-                            <p class="title">Title: ${movie.title}</p>
-                            <p class="director">Director: ${movie.director}</p>
-                            <p class="rating">Rating: ${movie.rating}</p>
-                            <p class="genre">Genre: ${movie.genre}</p>
-                            <p class="plot">Plot: ${movie.plot}</p>
+                    <div id="card" class="card-deck">
+                        <div class="card">
+                            <img class="card-img-top" src=".../100px180/" alt="Movie Title">
+                            <div class="card-body">
+                                <h5 class="card-title">${movie.title}</h5>
+                                <p class="director">Director: ${movie.director}></p>
+                                <p class="rating">Rating: ${movie.rating}</p>
+                                <p class="genre">Genre: ${movie.genre}</p>
+                                <p class="plot">Plot: ${movie.plot}</p>
+                            </div>
+                            <div class="card-footer">
+                                <small class="text-muted">Add buttons here soon</small>
+                            </div>
                         </div>
                     </div>
                 `;
@@ -32,6 +38,7 @@ function fetchMovies() {
         });
 }
 fetchMovies();
+
 
 
 
