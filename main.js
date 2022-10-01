78af39490471378563eb661b0c94ea8ab8dc3f6c
Estefania Argueta <109987491+3arglop@users.noreply.github.com>
2022-10-01 17:28:49 -0500

    changing fetch from Glitch to OMDB, but I have an invalid key. (#11)
    Co-authored-by: Casanova Geary <CasanovaGeary@Gmail.com>
    Co-authored-by: Casanova Geary <100623396+5T0P5331N6M3@users.noreply.github.com>

--- main.js
+++ main.js
@@ -1,8 +1,48 @@
 "use strict";
 
 /* FETCH GLITCH API */
+// function fetchFavoriteMovies() {
+//     fetch('https://aluminum-coral-comic.glitch.me/movies')
+//         .then(response => {
+//             console.log(response);
+//             // const data = response.json();
+//             if (!response.ok) {
+//                 throw Error('ERROR');
+//             }
+//             return response.json();
+//         })
+//         .then(data => {
+//             console.log(data);
+//             const favoriteHTML = data.map(favMovie => {
+//                 return `
+//                         <!-- Movie Card -->
+//                     <div id="favCard" class="card" style="margin: 4rem;">
+//                         <div class="fav-card-content">
+//                         <h5 class="fav-title">${favMovie.title}</h5>
+//                             <div class="fav-body">
+//                                 <img class="fav-poster" src="${favMovie.poster}" alt="Movie Title" style="width: 250px; height: 30vh;">
+//                                 <p class="fav-director">Director: ${favMovie.director}</p>
+//                                 <p class="fav-rated">Rated: ${favMovie.rated}</p>
+//                                 <p class="fav-genre">Genre: ${favMovie.genre}</p>
+//                                  <p class="fav-rating">Rating: ${favMovie.rating}</p>
+//                             </div>
+//                             <div class=" card-footer fav-footer">
+//                                 <button type="button" id="edit">Edit</button>
+//                                 <button type="button" id="delete">Delete</button>
+//                             </div>
+//                         </div>
+//                     </div>
+//                 `;
+//             })
+//                 .join("");
+//             // console.log(html);
+//             $('#favoriteWatchlist').html(favoriteHTML);
+//         });
+// }
+// fetchFavoriteMovies();
+
 function fetchFavoriteMovies() {
-    fetch('https://aluminum-coral-comic.glitch.me/movies')
+    fetch('https://www.omdbapi.com/?t=${title}&apikey=${OMDB_KEY}')
         .then(response => {
             console.log(response);
             // const data = response.json();
