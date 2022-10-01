75603c4863cedf0ddbccb8921b247059e0291178
3arglop <estefania.arguetalopez13@gmail.com>
2022-09-30 20:46:55 -0500

    created the searchMovies function to call the OMDb API when the user searches a movie using our input and button tags

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 "use strict";
 
 /* FETCH GLITCH API */
-function fetchMovies() {
+function fetchFavoriteMovies() {
     fetch('https://aluminum-coral-comic.glitch.me/movies')
         .then(response => {
             console.log(response);
@@ -13,18 +13,18 @@ function fetchMovies() {
         })
         .then(data => {
             console.log(data);
-            const html = data.map(movie => {
+            const favoriteHTML = data.map(favMovie => {
                 return `
                         <!-- Movie Card -->
                     <div id="card" class="card-deck" style="margin: 4rem;">
                         <div class="card">
-                        <h5 class="card-title">${movie.title}</h5>
+                        <h5 class="card-title">${favMovie.title}</h5>
                             <div class="card-body">
-                                <img class="card-img-top" src="${movie.poster}" alt="Movie Title" style="width: 250px; height: 30vh;">
-                                <p class="director">Director: ${movie.director}</p>
-                                <p class="rated">Rated: ${movie.rated}</p>
-                                <p class="genre">Genre: ${movie.genre}</p>
-                                 <p class="rating">Rating: ${movie.rating}</p>
+                                <img class="card-img-top" src="${favMovie.poster}" alt="Movie Title" style="width: 250px; height: 30vh;">
+                                <p class="director">Director: ${favMovie.director}</p>
+                                <p class="rated">Rated: ${favMovie.rated}</p>
+                                <p class="genre">Genre: ${favMovie.genre}</p>
+                                 <p class="rating">Rating: ${favMovie.rating}</p>
                             </div>
                             <div class="card-footer">
                                 <button type="button" id="edit">Edit</button>
@@ -36,10 +36,10 @@ function fetchMovies() {
             })
                 .join("");
             // console.log(html);
-            $('#favoriteWatchlist').html(html);
+            $('#favoriteWatchlist').html(favoriteHTML);
         });
 }
-fetchMovies();
+fetchFavoriteMovies();
 
 /* LOADER */
 document.onreadystatechange = function () {
@@ -54,7 +54,64 @@ document.onreadystatechange = function () {
     }
 };
 
-/* EVENT LISTENERS */
-$('#find-movie').on('click', function () {
-    alert("Search button works.");
+
+//CALLING DOM WITH #searchText (input) and .find-movie (button)
+$(document).ready(() => {
+    $('.find-movie').on('click', (e) => {
+        let title = $('#searchText').val();
+        searchMovies(title);
+        e.preventDefault();
+    });
 });
+
+
+//FETCH OMDb API TO SEARCH NEW MOVIES
+function searchMovies(title) {
+    fetch(`https://www.omdbapi.com/?t=${title}&apikey=${OMDB_KEY}`)
+        .then(response => {
+            console.log(response);
+            // const data = response.json();
+            if (!response.ok) {
+                throw Error('ERROR');
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log(data);
+            const searchData = data;
+            let searchOutput = '';
+            $.each(searchData, (index, newMovie) => {
+                searchOutput =+ `<div id="card" class="card-deck" style="margin: 4rem;">
+                        <div class="card">
+                        <h5 class="card-title">${newMovie.title}</h5>
+                            <div class="card-body">
+                                <img class="card-img-top" src="${newMovie.poster}" alt="Movie Title" style="width: 250px; height: 30vh;">
+                                <p class="director">Director: ${newMovie.director}</p>
+                                <p class="rated">Rated: ${newMovie.rated}</p>
+                                <p class="genre">Genre: ${newMovie.genre}</p>
+                                 <p class="rating">Rating: ${newMovie.rating}</p>
+                            </div>
+                            <div class="card-footer">
+                                <button type="button" id="edit">Edit</button>
+                                <button type="button" id="delete">Delete</button>
+                            </div>
+                        </div>
+                    </div>`;
+            });
+
+            $('#searchMovies').html(searchOutput);
+
+        }).catch((err) => {
+            console.log(err);
+    });
+}
+
+// searchMovies("toy story");
+// searchMovies("300");
+// searchMovies("crazy, stupid, love");
+
+
+// /* EVENT LISTENERS */
+// $('#find-movie').on('click', function () {
+//     alert("Search button works.");
+// });
